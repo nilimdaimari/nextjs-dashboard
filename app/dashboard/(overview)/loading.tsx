fe567c44cafe79a89d254a8cfebe57b566522535
nilimdaimari <daimarinilim@gmail.com>
2023-11-13 23:53:58 +0530

    updated

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+import React from "react";
+
+export default function loading() {
+  return (
+    <>
+      <div>
+        <DashboardSkeleton />
+      </div>
+    </>
+  );
+}
